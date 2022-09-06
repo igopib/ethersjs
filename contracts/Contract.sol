f4b124db53baacf430e09b0dff428972f486b5c9
Gurpreet <gs109690@gmail.com>
2022-09-06 17:27:26 -0500

    updates

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.7;
 
 contract Storage {
     uint256 number;
+    uint256 public iNumber;
+
+    event Increment(uint256 indexed eNumber);
 
     function addNumber(uint256 _number) public {
         number = _number;
@@ -10,5 +13,10 @@ contract Storage {
 
     function getNumber() public view returns (uint256) {
         return number;
+    }
+
+    function incrementNumber() public {
+        iNumber++;
+        emit Increment(iNumber);
     }
 }
--- frontend/ethers.js
+++ frontend/ethers.js
@@ -1,11 +1,30 @@
 const connectButton = document.getElementById("connectButton");
+const incrementNumber = document.getElementById("incrementButton");
+
 connectButton.onclick = connectWallet;
+incrementNumber.onclick = incrementNumber;
 
 let provider = new ethers.providers.Web3Provider(window.ethereum);
 let signer;
 
+// Fires up metamask connection
 async function connectWallet() {
   await provider.send("eth_requestAccounts", []);
   signer = provider.getSigner();
-  connectButton.innerHTML("Connected");
+  connectButton.innerHTML = "Connected";
+}
+
+async function incrementNumber() {
+  const numberContractAddress = ""; // Input contract address here.
+
+  const contractAbi = ["function incrementNumber() public "];
+
+  const icoContract = new ethers.Contract(
+    numberContractAddress,
+    contractAbi,
+    provider
+  );
+
+  const txResponse = await icoContract.connect(signer).incrementNumber();
+  await txResponse.wait();
 }
